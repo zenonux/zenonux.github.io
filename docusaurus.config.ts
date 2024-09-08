6015c7949f797dc9e53d49ada4af00671f5da2d9
换个头像心好累 <zenonux@outlook.com>
2024-09-08 09:20:10 +0800

    upfate searh

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,72 +1,72 @@
-import { themes as prismThemes } from "prism-react-renderer";
-import type { Config } from "@docusaurus/types";
-import type * as Preset from "@docusaurus/preset-classic";
+import { themes as prismThemes } from 'prism-react-renderer'
+import type { Config } from '@docusaurus/types'
+import type * as Preset from '@docusaurus/preset-classic'
 
 const config: Config = {
-  title: "Blog",
-  tagline: "Dinosaurs are cool",
-  favicon: "img/favicon.ico",
+  title: 'Blog',
+  tagline: 'Dinosaurs are cool',
+  favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: "https://zenonux.github.io",
+  url: 'https://zenonux.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "zenonux", // Usually your GitHub org/user name.
-  projectName: "zenonux.github.io", // Usually your repo name.
+  organizationName: 'zenonux', // Usually your GitHub org/user name.
+  projectName: 'zenonux.github.io', // Usually your repo name.
 
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
 
   presets: [
     [
-      "classic",
+      'classic',
       {
         docs: false,
         blog: {
-          blogTitle: "Blog",
-          blogDescription: "tech blog",
-          blogSidebarTitle: "All posts",
-          blogSidebarCount: "ALL",
-          routeBasePath: "/",
+          blogTitle: 'Blog',
+          blogDescription: 'tech blog',
+          blogSidebarTitle: 'All posts',
+          blogSidebarCount: 'ALL',
+          routeBasePath: '/',
           showLastUpdateTime: true,
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: "./src/css/custom.css",
+          customCss: './src/css/custom.css',
         },
       } satisfies Preset.Options,
     ],
   ],
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: "Blog",
+      title: 'Blog',
       logo: {
-        alt: "Blog",
-        src: "img/logo.svg",
+        alt: 'Blog',
+        src: 'img/logo.svg',
       },
       items: [
         {
-          href: "https://github.com/zenonux/zenonux.github.io",
-          label: "GitHub",
-          position: "right",
+          href: 'https://github.com/zenonux/zenonux.github.io',
+          label: 'GitHub',
+          position: 'right',
         },
       ],
     },
@@ -79,35 +79,35 @@ const config: Config = {
     },
     algolia: {
       // The application ID provided by Algolia
-      appId: "N0B69B3KTM",
+      appId: '2COAZ9ORA7',
 
       // Public API key: it is safe to commit it
-      apiKey: "81b1b1ad2c18d2488987c42c314070b7",
+      apiKey: 'cd6d536d63383698497344f6a8b05fec',
 
-      indexName: "blog",
+      indexName: 'blog',
 
       // Optional: see doc section below
       contextualSearch: true,
 
       // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-      externalUrlRegex: "external\\.com|domain\\.com",
+      externalUrlRegex: 'external\\.com|domain\\.com',
 
       // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
       replaceSearchResultPathname: {
-        from: "/docs/", // or as RegExp: /\/docs\//
-        to: "/",
+        from: '/docs/', // or as RegExp: /\/docs\//
+        to: '/',
       },
 
       // Optional: Algolia search parameters
       searchParameters: {},
 
       // Optional: path for search page that enabled by default (`false` to disable it)
-      searchPagePath: "search",
+      searchPagePath: 'search',
 
       // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
       insights: false,
     },
   } satisfies Preset.ThemeConfig,
-};
+}
 
-export default config;
+export default config
